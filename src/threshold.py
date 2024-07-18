a82d860f99a95254815096851ef2f21a5406548e
Sascha Brawer <sascha@brawer.ch>
2024-07-18 15:57:04 +0200

    Emit thresholded TIFFs in tiled format

--- src/threshold.py
+++ src/threshold.py
@@ -15,6 +15,8 @@
 
 import json
 import os
+import subprocess
+import tempfile
 import warnings
 
 import cv2
@@ -27,73 +29,76 @@ OUTPUT_DPI = 600
 
 def threshold(in_path, out_path):
     print("thresholding", in_path)
-    out_pages = []
-    with PIL.Image.open(in_path) as tiff:
-        out_pages = []
-        for page_num in range(tiff.n_frames):
-            tiff.seek(page_num)
-            if d := tiff.tag_v2.get(270):
-                meta = json.loads(d)
-            else:
-                meta = {}
-            date_time = tiff.tag_v2.get(306, "")
-            x_dpi, y_dpi = tiff.info["dpi"]
-            page = numpy.asarray(tiff)
-            out_width = int(page.shape[1] * OUTPUT_DPI / x_dpi + 0.5)
-            out_height = int(page.shape[0] * OUTPUT_DPI / y_dpi + 0.5)
-            out_size = (out_width, out_height)
-            scaled = cv2.resize(page, out_size, interpolation=cv2.INTER_LINEAR)
-            blurred = cv2.bilateralFilter(scaled, 9, 75, 75)
-            gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-            t, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            meta["thresholding"] = {
-                "threshold": t,
-                "method": "cv2.BINARY|OTSU",
-            }
+    # As of July 2024, the pillow library is able to write multi-page TIFFs,
+    # but tiling does not seem to be implemented. Therefore we write out
+    # an untiled multi-page TIFF and then run the `tiffcp` command to
+    # produce a tiled image.
+    with tempfile.TemporaryDirectory() as tmp:
+        tmp_path = os.path.join(tmp, "t.tif")
+        with open(tmp_path, "w+b") as fp, PIL.TiffImagePlugin.AppendingTiffWriter(
+            fp
+        ) as out_tiff, PIL.Image.open(in_path) as tiff:
+            for page_num in range(tiff.n_frames):
+                tiff.seek(page_num)
+                page = threshold_page(tiff, page_num)
+                # https://github.com/python-pillow/Pillow/issues/3636#issuecomment-461986355
+                PIL.TiffImagePlugin._save(page, out_tiff, tmp_path)
+                out_tiff.newFrame()
+        cmd = ["tiffcp", "-t", "-w", "512", "-l", "512", tmp_path, out_path]
+        subprocess.run(cmd)
 
-            # For typical good scans, the computed threshold (t) is above 140.
-            # However, some scans are very dark, for example the first page
-            # of mutation AA3008 with t=83. In those cases, we use a threshold
-            # that artificially darkens the image, in the hope that some circles
-            # (for border points) stay closed.
-            if t < 110:  # eg. first page of mutation AA3008 has t=83
-                new_t = t + 30
-                meta["thresholding"] = {
-                    "value": new_t,
-                    "otsu_value": t,
-                    "method": "cv2.BINARY",
-                }
-                t, thresh = cv2.threshold(gray, new_t, 255, cv2.THRESH_BINARY)
-                warnings.warn("fixing up thresholding: %s" % in_path)
 
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-            thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-            width, height = thresh.shape[1], thresh.shape[0]
-            cv2.rectangle(thresh, (0, 0), (width - 1, height - 1), 255, 1)
+def threshold_page(tiff, page_num):
+    if d := tiff.tag_v2.get(270):
+        meta = json.loads(d)
+    else:
+        meta = {}
+    date_time = tiff.tag_v2.get(306, "")
+    x_dpi, y_dpi = tiff.info["dpi"]
+    page = numpy.asarray(tiff)
+    out_width = int(page.shape[1] * OUTPUT_DPI / x_dpi + 0.5)
+    out_height = int(page.shape[0] * OUTPUT_DPI / y_dpi + 0.5)
+    out_size = (out_width, out_height)
+    scaled = cv2.resize(page, out_size, interpolation=cv2.INTER_LINEAR)
+    blurred = cv2.bilateralFilter(scaled, 9, 75, 75)
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    t, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    meta["thresholding"] = {
+        "threshold": t,
+        "method": "cv2.BINARY|OTSU",
+    }
 
-            bw = PIL.Image.fromarray(thresh).convert("1")
-            bw.encoderinfo = {
-                "compression": "group4",
-                "date_time": date_time,
-                "dpi": (OUTPUT_DPI, OUTPUT_DPI),
-                # For custom tags:
-                # 'tiffinfo': PIL.TiffImagePlugin.ImageFileDirectory(),
-            }
+    # For typical good scans, the computed threshold (t) is above 140.
+    # However, some scans are very dark, for example the first page
+    # of mutation AA3008 with t=83. In those cases, we use a threshold
+    # that artificially darkens the image, in the hope that some circles
+    # (for border points) stay closed.
+    if t < 110:  # eg. first page of mutation AA3008 has t=83
+        new_t = t + 30
+        meta["thresholding"] = {
+            "value": new_t,
+            "otsu_value": t,
+            "method": "cv2.BINARY",
+        }
+        t, thresh = cv2.threshold(gray, new_t, 255, cv2.THRESH_BINARY)
+        warnings.warn("fixing up thresholding: %s" % in_path)
 
-            desc = json.dumps(meta, sort_keys=True, separators=(",", ":"))
-            bw.encoderinfo["description"] = desc
-            if date_time:
-                bw.encoderinfo["date_time"] = date_time
-            out_pages.append(bw)
+    width, height = thresh.shape[1], thresh.shape[0]
+    bw = PIL.Image.fromarray(thresh).convert("1")
+    bw.encoderconfig = ()
+    bw.encoderinfo = {
+        "compression": "group4",
+        "date_time": date_time,
+        "dpi": (OUTPUT_DPI, OUTPUT_DPI),
+        # For custom tags:
+        # 'tiffinfo': PIL.TiffImagePlugin.ImageFileDirectory(),
+    }
 
-    # https://github.com/python-pillow/Pillow/issues/3636#issuecomment-461986355
-    with open(out_path, "w+b") as fp:
-        with PIL.TiffImagePlugin.AppendingTiffWriter(fp) as tiff:
-            for page in out_pages:
-                page.encoderconfig = ()
-                PIL.TiffImagePlugin._save(page, tiff, out_path)
-                tiff.newFrame()
+    desc = json.dumps(meta, sort_keys=True, separators=(",", ":"))
+    bw.encoderinfo["description"] = desc
+    if date_time:
+        bw.encoderinfo["date_time"] = date_time
+    return bw
 
 
 if __name__ == "__main__":
