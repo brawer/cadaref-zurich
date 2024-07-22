a64f84c308614272d3478be0cc2eb54a125606a6
Sascha Brawer <sascha@brawer.ch>
2024-07-22 17:01:58 +0200

    Extract survey mutation ID and creation date

--- src/extract.py
+++ src/extract.py
@@ -28,67 +28,88 @@ def sortkey(s):
         return (s, 0, "")
 
 
-def extract_parcels(gpkg):
+def extract_parcels(gpkg, mutations):
     cursor = gpkg.connection.execute(
-        "SELECT nummer, geom '[Polygon]' FROM 'geoz_2007.av_li_liegenschaft_a'"
+        "SELECT nummer, geom '[Polygon]', entstehung FROM 'geoz_2007.av_li_liegenschaft_a'"
     )
     parcels = {}
-    for num, geom in cursor.fetchall():
+    for num, geom, mut in cursor.fetchall():
         assert geom.srs_id == 2056  # epsg.io/2056 = Swiss CH1903+/LV95
         e = geom.envelope
+        mut_id, mut_date = mutations[int(mut)]
         parcels[num] = (
             int(e.min_x),
             int(e.max_x + 0.5),
             int(e.min_y),
             int(e.max_y + 0.5),
+            mut_id,
+            mut_date,
         )
     with open("parcels.csv", "w") as out:
         w = csv.writer(out)
-        w.writerow(["parcel", "min_x", "max_x", "min_y", "max_y"])
+        w.writerow(
+            ["parcel", "min_x", "max_x", "min_y", "max_y", "created_by", "created"]
+        )
         for key in sorted(parcels, key=sortkey):
             row = [key] + [str(c) for c in parcels[key]]
             w.writerow(row)
 
 
-def extract_border_points(gpkg):
+def extract_mutations(gpkg):
+    mutations = {}
+    for obj_id, mut_id, date in gpkg.connection.execute(
+        "SELECT objid, identifikator, gueltigereintrag "
+        "FROM 'geoz_2007.av_li_lsnachfuehrung'"
+    ):
+        date = str(int(date))
+        assert len(date) == 8, date
+        date = date[:4] + "-" + date[4:6] + "-" + date[6:8]
+        mutations[int(obj_id)] = (mut_id, date)
+    return mutations
+
+
+def extract_border_points(gpkg, mutations):
     points = {}
-    for id, geom, kind in gpkg.connection.execute(
-        "SELECT identifikator, geom '[Point]', punktzeichen_txt "
+    for id, geom, kind, mut in gpkg.connection.execute(
+        "SELECT identifikator, geom '[Point]', punktzeichen_txt, entstehung "
         "FROM 'geoz_2007.av_li_grenzpunkt'"
     ):
         assert id not in points, id
         assert geom.srs_id == 2056, id  # epsg.io/2056 = Swiss CH1903+/LV95
-        points[id] = (id, kind, geom.x, geom.y)
+        mut_id, mut_date = mutations[int(mut)]
+        points[id] = (id, kind, geom.x, geom.y, mut_id, mut_date)
     with open("border_points.csv", "w") as out:
         w = csv.writer(out)
-        w.writerow(["point", "type", "x", "y"])
+        w.writerow(["point", "type", "x", "y", "created_by", "created"])
         for key in sorted(points, key=sortkey):
-            id, kind, x, y = points[key]
-            w.writerow([id, kind, "%.3f" % x, "%.3f" % y])
+            id, kind, x, y, mut_id, mut_date = points[key]
+            w.writerow([id, kind, "%.3f" % x, "%.3f" % y, mut_id, mut_date])
 
 
-def extract_fixed_points(gpkg):
+def extract_fixed_points(gpkg, mutations):
     points = {}
     for table in ("av_fi_lfp1", "av_fi_lfp2", "av_fi_lfp3"):
-        for id, geom, kind, protection in gpkg.connection.execute(
-            "SELECT nummer, geom '[Point]', punktzeichen_txt, schutz_txt "
+        for id, geom, kind, protection, mut in gpkg.connection.execute(
+            "SELECT nummer, geom '[Point]', punktzeichen_txt, schutz_txt, entstehung "
             + "FROM 'geoz_2007.%s'" % table
         ):
             assert id not in points, id
             assert geom.srs_id == 2056, id  # epsg.io/2056 = Swiss CH1903+/LV95
-            points[id] = (id, kind, protection, geom.x, geom.y)
+            mut_id, mut_date = mutations[int(mut)]
+            points[id] = (id, kind, protection, geom.x, geom.y, mut_id, mut_date)
     with open("fixed_points.csv", "w") as out:
         w = csv.writer(out)
-        w.writerow(["point", "type", "protection", "x", "y"])
+        w.writerow(["point", "type", "protection", "x", "y", "created_by", "created"])
         for key in sorted(points, key=sortkey):
-            id, kind, protection, x, y = points[key]
+            id, kind, protection, x, y, mut_id, mut_date = points[key]
             if protection == "kein_Schutz":
                 protection = ""
-            w.writerow([id, kind, protection, "%.3f" % x, "%.3f" % y])
+            w.writerow([id, kind, protection, "%.3f" % x, "%.3f" % y, mut_id, mut_date])
 
 
 if __name__ == "__main__":
     gpkg = GeoPackage("av2007.gpkg")
-    extract_border_points(gpkg)
-    extract_fixed_points(gpkg)
-    extract_parcels(gpkg)
+    mutations = extract_mutations(gpkg)
+    extract_border_points(gpkg, mutations)
+    extract_fixed_points(gpkg, mutations)
+    extract_parcels(gpkg, mutations)
