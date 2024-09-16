c5ddfd53022d8ae8d4d1cdb4144e9dd8077b9026
Sascha Brawer <sascha@brawer.ch>
2024-09-16 14:45:22 +0200

    WIP: Find search area for geo points in georeferencing

--- src/georeference_v2.py
+++ src/georeference_v2.py
@@ -3,6 +3,8 @@
 
 from collections import namedtuple
 import csv
+import io
+import json
 import os
 import re
 
@@ -10,6 +12,7 @@ import cv2
 import json
 import numpy
 import PIL.Image
+import quads
 
 from classify import detect_map_symbols
 from threshold import threshold
@@ -17,13 +20,28 @@ from threshold import threshold
 Mutation = namedtuple("Mutation", "id date min_x max_x min_y max_y")
 Parcel = namedtuple("Parcel", "id min_x max_x min_y max_y")
 DeletedPoint = namedtuple("DeletedPoint", "id style x y created_by deleted_by")
+BorderPoint = namedtuple("BorderPoint", "id style x y created")
+FixedPoint = namedtuple("FixedPoint", "id style x y created")
 
 
-# point class in src/deleted_points.csv --> cartographic style
+# Map from point classes in survey_data/border_points.csv to cartographic
+# styles. Style IDs are the same as returned by detect_map_symbols()
+# in src/classify.py.
+BORDER_POINT_STYLES = {
+    "unversichert": "black_dot",
+    "Bolzen": "white_circle",
+    "Stein": "white_circle",
+}
+
+
+# Map from point classes in src/deleted_points.csv to cartographic styles.
+# Cartographic style IDs are the same as returned by detect_map_symbols()
+# in src/classify.py.
 DELETED_POINT_STYLES = {
     "2": "double_white_circle",
     "4": "white_circle",
 }
+
 
 # The PDFs with the scanned mutation files indicate as part of their
 # file names which parcels have been created by each mutation. For
@@ -54,13 +72,22 @@ PARCEL_RE = re.compile(
 )
 
 
+# Regular expression for extracting the map/plan scale from OCRed plaintext.
+PLAN_SCALE_RE = re.compile(r"\b1\s*:\s*(100|200|250|500|1000|2000)\b")
+
+
 class Georeferencer(object):
     def __init__(self):
         self.deleted_points = self._read_deleted_points()
         self.mutations = self._read_mutations()
         self.parcels = self._read_parcels()
+        points = list(self._read_fixed_points())
+        points.extend(self._read_border_points())
+        points.extend(self.deleted_points)
+        self.quad_tree = self._build_quad_tree(points)
 
     def georeference(self, mutation):
+        log = io.StringIO()
         rendered_path = os.path.join("rendered", f"{mutation}.tif")
         thresholded_path = os.path.join("thresholded", f"{mutation}.tif")
         if not os.path.exists(thresholded_path):
@@ -68,11 +95,104 @@ class Georeferencer(object):
         with open(os.path.join("rendered", f"{mutation}.txt")) as fp:
             ocr_text = fp.read()
             ocr_parcels = set([m[0] for m in PARCEL_RE.findall(ocr_text)])
-        with PIL.Image.open(thresholded_path) as thresh:
-            bboxes = self._mutation_bboxes(mutation, thresh, ocr_parcels)
-            symbols = self._detect_map_symbols(mutation, thresh, scale=0.5)
-        if len(bboxes) == 0:
-            print(mutation)
+            ocr_scales = self._extract_scales(ocr_text)
+            screenshots = self._screenshot_pages(ocr_text)
+        with PIL.Image.open(rendered_path) as rend:
+            with PIL.Image.open(thresholded_path) as thresh:
+                bbox = self._mutation_bbox(mutation, thresh, ocr_parcels)
+                for page_num in range(thresh.n_frames):
+                    rend.seek(page_num)
+                    thresh.seek(page_num)
+                    meta_json = thresh.tag_v2[270]
+                    meta = json.loads(meta_json)
+                    log.write(f"#### Cadaref {meta_json}\n")
+                    log.write(f"ocr_parcels: {ocr_parcels}\n")
+                    log.write(f"bbox: {bbox}\n")
+                    if not bbox:
+                        log.write("status: cannot guess approx location\n")
+                        continue
+
+                    rendered_dpi = float(rend.info["dpi"][0])
+                    thresholded_dpi = float(thresh.info["dpi"][0])
+                    symbols = self._detect_map_symbols(
+                        mutation, thresh, scale=rendered_dpi / thresholded_dpi
+                    )
+                    log.write("num_symbols: %d\n" % len(symbols))
+                    log.write("symbols: %s\n" % symbols)
+                    if len(symbols) < 4:
+                        log.write("status: not enough symbols\n")
+                        continue
+
+                    # Our rendering phase in src/render.py tries to
+                    # find A3 pages that have (perhaps mistakenly)
+                    # been glued together during scanning, and splits
+                    # them in a left and right half based on a
+                    # page-splitting heuristics. However, OCR does not
+                    # know about this page splitting. Therefore, for
+                    # the OCR-derived heuristics, we need to
+                    # reconstruct the page kay before splitting. For
+                    # example, pages "7L" and "7R" both have page
+                    # number 7 in the view of OCR.
+                    scan, scan_page = meta["scan"], meta["scan_page"]
+                    if scan_page[-1] in {"L", "R"}:
+                        page_key = (scan, int(scan_page[:-1]))
+                    else:
+                        page_key = (scan, int(scan_page))
+
+                    if page_key in screenshots:
+                        log.write("status: screenshot\n")
+                        continue
+
+                    # Find an approximate area for the scanned plan.
+                    # In this area, we look for geo points that might
+                    # be matchable against the detected map symbols.
+                    scale = ocr_scales.get(page_key)
+                    width_cm = (thresh.width / thresholded_dpi) * 2.54
+                    height_cm = (thresh.height / thresholded_dpi) * 2.54
+                    if scale is not None:
+                        width_m = (width_cm / 100.0) * scale
+                        height_m = (height_cm / 100.0) * scale
+                    else:
+                        width_m = (width_cm / 100.0) * 2000
+                        height_m = (height_cm / 100.0) * 2000
+                    min_x, max_x, min_y, max_y = bbox
+                    width_m = max(width_m, max_x - min_x)
+                    height_m = max(height_m, max_y - min_y)
+                    center_x = min_x + (max_x - min_y) / 2
+                    center_y = min_y + (max_y - min_y) / 2
+                    # We don't know the plan rotation, so we take the max
+                    # of width and height. Typical search radius is ~3.5 km.
+                    search_radius = max(width_m, height_m) / 2
+                    search_bbox = quads.BoundingBox(
+                        min_x=center_x - search_radius,
+                        max_x=center_x + search_radius,
+                        min_y=center_y - search_radius,
+                        max_y=center_y + search_radius,
+                    )
+                    log.write(f"search_radius: {search_radius}\n")
+                    log.write(f"search_bbox: {search_bbox}\n")
+                    # TODO: Look up search_bbox in quadtree
+                    # TODO: Pass everything to cadaref tool
+                    print("*** ZEBRA-B", mutation, search_bbox, scale, len(symbols))
+
+                    log.write(f"scale: {scale}\n")
+
+                    # print(scan, scan_page, scale, len(symbols), max_x - min_x, max_y-min_y)
+
+            self._write_log(mutation, log.getvalue())
+
+    @staticmethod
+    def _write_log(mutation, message):
+        # We write the log to a temporary file (with a different name),
+        # which is not an atomic operation and could be interrupted.
+        # Once the log file is completely written to disk, we rename
+        # the temporary file to the final file name in an atomic operation.
+        # This ensures we don't end up with partially written log files.
+        log_path = os.path.join("georeferenced", f"{mutation}.log")
+        tmp_path = log_path + ".tmp"
+        with open(tmp_path, "w") as f:
+            f.write(message)
+        os.rename(tmp_path, log_path)
 
     def _read_mutations(self):
         points, dates = {}, {}
@@ -142,6 +262,48 @@ class Georeferencer(object):
                 )
         return points
 
+    @staticmethod
+    def _read_border_points():
+        path = os.path.join("survey_data", "border_points.csv")
+        with open(path) as fp:
+            for rec in csv.DictReader(fp):
+                if style := BORDER_POINT_STYLES.get(rec["type"]):
+                    yield BorderPoint(
+                        id=rec["point"],
+                        style=style,
+                        x=float(rec["x"]),
+                        y=float(rec["y"]),
+                        created=rec["created"],
+                    )
+
+    @staticmethod
+    def _read_fixed_points():
+        path = os.path.join("survey_data", "fixed_points.csv")
+        with open(path) as fp:
+            for rec in csv.DictReader(fp):
+                yield FixedPoint(
+                    id=rec["point"],
+                    style="double_white_circle",
+                    x=float(rec["x"]),
+                    y=float(rec["y"]),
+                    created=rec["created"],
+                )
+
+    @staticmethod
+    def _build_quad_tree(points):
+        min_x, max_x = min(p.x for p in points), max(p.x for p in points)
+        min_y, max_y = min(p.y for p in points), max(p.y for p in points)
+        width, height = max_x - min_x, max_y - min_y
+        center_x, center_y = min_x + width / 2.0, min_y + height / 2.0
+        tree = quads.QuadTree(
+            center=(center_x, center_y),
+            width=width + 1.0,
+            height=height + 1.0,
+        )
+        for p in points:
+            tree.insert((p.x, p.y), data=p)
+        return tree
+
     def _mutation_parcels(self, tiff):
         parcels = set()
         for page_num in range(tiff.n_frames):
@@ -150,34 +312,37 @@ class Georeferencer(object):
             parcels.update(meta.get("parcels", []))
         return parcels
 
-    def _mutation_bboxes(self, mutation, tiff, ocr_parcels):
+    def _mutation_bbox(self, mutation, tiff, ocr_parcels):
         boxes = []
         meta = json.loads(tiff.tag_v2[270])
+
         # Sometimes we know a mutation's coordinates from survey data,
         # such as when the current survey data contains border points
         # that have been created by a mutation.
         if m := self.mutations.get(meta["mutation"]):
             boxes.append(m)
+
         # Another source for coordinates is the parcels that were
         # created by the mutation; some of the newly created parcels
         # may still exist today.
         for parcel in self._mutation_parcels(tiff):
             if p := self.parcels.get(parcel):
                 boxes.append(p)
+
         # Also, the mutation PDF may contain parcel names which
         # have been extracted from OCR. For example, newer plans
         # conain strings such as "WO3525" which look very much
         # like a parcel name; again, some of those parcels may
         # still exist today so we get a bounding box from survey
-        # data.
-        for parcel in ocr_parcels:
-            if p := self.parcels.get(parcel):
-                boxes.append(p)
-        return boxes
+        # data. However, since OCR is less reliable than manual
+        # annotations, we the OCRed parcel names only if we have
+        # no better data source.
+        if len(boxes) == 0:
+            for parcel in ocr_parcels:
+                if p := self.parcels.get(parcel):
+                    boxes.append(p)
 
-    @staticmethod
-    def _bbox_extent(boxes):
-        if boxes is not None and len(boxes) > 0:
+        if len(boxes) > 0:
             min_x = min(b.min_x for b in boxes if b.min_x)
             max_x = max(b.max_x for b in boxes if b.max_x)
             min_y = min(b.min_y for b in boxes if b.min_y)
@@ -186,21 +351,77 @@ class Georeferencer(object):
         else:
             return None
 
+    # Extract the plan/map scales from OCRed plaintext.
+    #
+    # The result is a map from (scan, page_num) to scale, for example
+    #   {('HG_Mut_21853_Kat_HG8332_HG8333_j2007.pdf', 1): 500}
+    # meaning page 1 of that pdf file contains the string "1:500".
+    #
+    # Caveats:
+    #
+    # * Some scans do not indicate their scale at all, but this is rare.
+    #
+    # * Sometimes, OCR fails to read the plan indicator.
+    #
+    # * Some scans have multiple plans with different scales, but always
+    #   on different pages.
+    #
+    # * OCR runs independent of page splitting, so we might return
+    #   a scale for page 7 whereas in the rendered TIFF file the plan
+    #   is on page "7L" or "7R" depending on whether the plan is
+    #   located on the left or right half of an A3 page.
+    def _extract_scales(self, ocr_text):
+        scales = {}
+        for page in ocr_text.split("#### Cadaref ")[1:]:
+            meta_json, page_text = page.split("\n", 1)
+            meta = json.loads(meta_json)
+            scan, scan_page = meta["scan"], meta["scan_page"]
+            if m := PLAN_SCALE_RE.search(page_text):
+                scales[(scan, scan_page)] = int(m.group(1))
+        return scales
+
+    # Some mutation files contain pages with screenshots of a Windows tool
+    # that confuse our symbol detection. We can identify these through OCR.
+    #
+    # For example, pages 12 and 13 of scan WO_Mut_20003_Kat_WO6495_j2005.pdf
+    # are screenhots of that Windows tool, and the result of calling this
+    # method on that scan is the following Python set.
+    #
+    # {
+    #     ('WO_Mut_20003_Kat_WO6495_j2005.pdf', 12),
+    #     ('WO_Mut_20003_Kat_WO6495_j2005.pdf', 13)
+    # }
+    @staticmethod
+    def _screenshot_pages(ocr_text):
+        screenshots = set()
+        for page in ocr_text.split("#### Cadaref ")[1:]:
+            meta_json, page_text = page.split("\n", 1)
+            if " VAZ-LB " in page_text:
+                meta = json.loads(meta_json)
+                scan, scan_page = meta["scan"], meta["scan_page"]
+                screenshots.add((scan, scan_page))
+        return screenshots
+
     def _detect_map_symbols(self, _mutation, thresh, scale):
-        result = []
-        for page_num in range(thresh.n_frames):
-            thresh.seek(page_num)
-            page = numpy.asarray(thresh).astype(numpy.uint8) * 255
-            # Our classifier sometimes gets confused if the outermost
-            # pixels aren't white. Draw a one-pixel white line around
-            # the plan.
-            h, w = page.shape[0], page.shape[1]
-            cv2.rectangle(page, (0, 0), (w - 1, h - 1), color=255)
-            symbols = [
-                (x * scale, y * scale, sym) for (x, y, sym) in detect_map_symbols(page)
-            ]
-            result.append(symbols)
-        return result
+        page = numpy.asarray(thresh).astype(numpy.uint8) * 255
+        # Our classifier sometimes gets confused if the outermost
+        # pixels aren't white. Draw a one-pixel white line around
+        # the plan.
+        h, w = page.shape[0], page.shape[1]
+        cv2.rectangle(page, (0, 0), (w - 1, h - 1), color=255)
+        # At the moment, detection of white symbols is much
+        # more reliable than detection of black symbols (mainly
+        # because small black dots get confused with text and
+        # dotted lines), so we restrict ourselves to white.
+        # It still generates enough fodder for matching,
+        # because black dots stand for unverified border points
+        # which are relatively rare in practice, at least within
+        # a city like Zürich.
+        return [
+            (x * scale, y * scale, sym)
+            for (x, y, sym) in detect_map_symbols(page)
+            if "white" in sym
+        ]
 
 
 if __name__ == "__main__":
@@ -208,6 +429,6 @@ if __name__ == "__main__":
     os.makedirs("georeferenced", exist_ok=True)
     ref = Georeferencer()
     mutations = set(f.rsplit(".", 1)[0] for f in os.listdir("rendered"))
-    mutations = {m for m in mutations if m[0] not in {"2", "3"}}
+    # mutations = {m for m in mutations if m[0] not in {"2", "3"}}
     for mut in sorted(mutations):
         ref.georeference(mut)
